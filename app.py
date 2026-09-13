2f9aa58eb7ce3a35d118d1d92cfed8d6f6815463
Sreenanda <ketone794@gmail.com>
2026-09-13 17:11:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, Response
-from invisibility import generate_frames
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,19 +8,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/video")
-def video():
-
-    return Response(
-        generate_frames(),
-        mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
-
-
 if __name__ == "__main__":
-
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=5000,
-        debug=True
+        debug=False
     )
